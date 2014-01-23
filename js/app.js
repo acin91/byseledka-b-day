7a71d8d87d8ef3d7e1a0dd4972ccfa8be75040fe
acin91 <z.aliakseyeu@gmail.com>
2014-01-23 15:27:34 +0300

    fix with js

--- js/app.js
+++ js/app.js
@@ -11,60 +11,61 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
 }
 
 
-// funny color toggling
-var color = '0000ff';
-var wrapper = $('#photo_wrap');
-function toggleColor() {
-    $(wrapper).css('background-color', '#' + color);
-    color = (color == '0000ff') ? '00ff00' : '0000ff';
-}
-
-setInterval(function() {
-    toggleColor()
-}, 200);
-
-// canvas eraser job
-var canvas = document.getElementById('magician');
-var image = document.getElementById('real_photo');
-
-$(image).on('load', function () {
-    var ctx = canvas.getContext('2d');
-    var width  = 375; var height = 500;
-    canvas.height = height; canvas.width = width;
-    var offset = $(canvas).offset();
-    offset.top = offset.top - height;
-    $(window).resize( function() {
-        offset = $(canvas).offset();
-        offset.top = offset.top - height;
-    });
-
-    with (ctx) {
-        lineCap = "round";
-        drawImage(image, 0, 0);
-        console.log(image);
-        globalCompositeOperation = "destination-out";
-        lineWidth = 15;
-        strokeStyle = "green";
+$(document).on('ready', function() {
+    // funny color toggling
+    var color = '0000ff';
+    var wrapper = $('#photo_wrap');
+    function toggleColor() {
+        $(wrapper).css('background-color', '#' + color);
+        color = (color == '0000ff') ? '00ff00' : '0000ff';
     }
 
-    image.remove();
+    setInterval(function() {
+        toggleColor()
+    }, 200);
 
-    canvas.addEventListener('mousedown', function(e) {
-        this.down = true;
-        this.X = e.pageX - offset.left;
-        this.Y = e.pageY - offset.top;
-    }, 0);
-    canvas.addEventListener('mouseup', function() {
-        this.down = false;          
-    }, 0);
-    canvas.addEventListener('mousemove', function(e) {
-        if (this.down) {
-            ctx.beginPath();
-            ctx.moveTo(this.X, this.Y);
-            ctx.lineTo(e.pageX - offset.left, e.pageY - offset.top);
-            ctx.stroke();
+    // canvas eraser job
+    $('#real_photo').on('load', function () {
+        var canvas = document.getElementById('magician');
+        var ctx = canvas.getContext('2d');
+        var width  = 375; var height = 500;
+        canvas.height = height; canvas.width = width;
+        var offset = $(canvas).offset();
+        offset.top = offset.top - height;
+        $(window).resize( function() {
+            offset = $(canvas).offset();
+            offset.top = offset.top - height;
+        });
+
+        with (ctx) {
+            lineCap = "round";
+            drawImage(image, 0, 0);
+            console.log(image);
+            globalCompositeOperation = "destination-out";
+            lineWidth = 15;
+            strokeStyle = "green";
+        }
+
+        image.remove();
+
+        canvas.addEventListener('mousedown', function(e) {
+            this.down = true;
             this.X = e.pageX - offset.left;
             this.Y = e.pageY - offset.top;
-        }
-    }, 0);
+        }, 0);
+        canvas.addEventListener('mouseup', function() {
+            this.down = false;          
+        }, 0);
+        canvas.addEventListener('mousemove', function(e) {
+            if (this.down) {
+                ctx.beginPath();
+                ctx.moveTo(this.X, this.Y);
+                ctx.lineTo(e.pageX - offset.left, e.pageY - offset.top);
+                ctx.stroke();
+                this.X = e.pageX - offset.left;
+                this.Y = e.pageY - offset.top;
+            }
+        }, 0);
+    });
+
 });
