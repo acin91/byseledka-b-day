b329e6b2e3b515cfee524d314959e4f64d8f3266
acin91 <z.aliakseyeu@gmail.com>
2014-01-23 15:34:52 +0300

    fix with coords in js. I think it will be last fix

--- js/app.js
+++ js/app.js
@@ -26,46 +26,50 @@ $(document).on('ready', function() {
 
     // canvas eraser job
     $('#real_photo').on('load', function () {
-        var canvas = document.getElementById('magician');
-        var ctx = canvas.getContext('2d');
-        var width  = 375; var height = 500;
-        canvas.height = height; canvas.width = width;
-        var offset = $(canvas).offset();
-        offset.top = offset.top - height;
-        $(window).resize( function() {
-            offset = $(canvas).offset();
-            offset.top = offset.top - height;
-        });
+        draw();
+    });
+});
 
-        with (ctx) {
-            lineCap = "round";
-            drawImage(image, 0, 0);
-            console.log(image);
-            globalCompositeOperation = "destination-out";
-            lineWidth = 15;
-            strokeStyle = "green";
-        }
-
-        image.remove();
-
-        canvas.addEventListener('mousedown', function(e) {
-            this.down = true;
-            this.X = e.pageX - offset.left;
-            this.Y = e.pageY - offset.top;
-        }, 0);
-        canvas.addEventListener('mouseup', function() {
-            this.down = false;          
-        }, 0);
-        canvas.addEventListener('mousemove', function(e) {
-            if (this.down) {
-                ctx.beginPath();
-                ctx.moveTo(this.X, this.Y);
-                ctx.lineTo(e.pageX - offset.left, e.pageY - offset.top);
-                ctx.stroke();
-                this.X = e.pageX - offset.left;
-                this.Y = e.pageY - offset.top;
-            }
-        }, 0);
+function draw()
+{
+    var image = document.getElementById('real_photo');
+    var canvas = document.getElementById('magician');
+    var ctx = canvas.getContext('2d');
+    var width  = 375; var height = 500;
+    canvas.height = height; canvas.width = width;
+    var offset = $(canvas).offset();
+    // offset.top = offset.top - height;
+    $(window).resize( function() {
+        offset = $(canvas).offset();
+        // offset.top = offset.top - height;
     });
 
-});
+    with (ctx) {
+        lineCap = "round";
+        drawImage(image, 0, 0);
+        globalCompositeOperation = "destination-out";
+        lineWidth = 15;
+        strokeStyle = "green";
+    }
+
+    image.remove();
+
+    canvas.addEventListener('mousedown', function(e) {
+        this.down = true;
+        this.X = e.pageX - offset.left;
+        this.Y = e.pageY - offset.top;
+    }, 0);
+    canvas.addEventListener('mouseup', function() {
+        this.down = false;          
+    }, 0);
+    canvas.addEventListener('mousemove', function(e) {
+        if (this.down) {
+            ctx.beginPath();
+            ctx.moveTo(this.X, this.Y);
+            ctx.lineTo(e.pageX - offset.left, e.pageY - offset.top);
+            ctx.stroke();
+            this.X = e.pageX - offset.left;
+            this.Y = e.pageY - offset.top;
+        }
+    }, 0);
+}
