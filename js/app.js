4d8239f6f596cf14e4d8f2c016553d6d093ea045
acin91 <z.aliakseyeu@gmail.com>
2014-01-23 15:25:21 +0300

    try to fix

--- js/app.js
+++ js/app.js
@@ -25,43 +25,46 @@ setInterval(function() {
 
 // canvas eraser job
 var canvas = document.getElementById('magician');
-var ctx = canvas.getContext('2d');
-var width  = 375; var height = 500;
-canvas.height = height; canvas.width = width;
 var image = document.getElementById('real_photo');
-var offset = $(canvas).offset();
-offset.top = offset.top - height;
-$(window).resize( function() {
-    offset = $(canvas).offset();
+
+$(image).on('load', function () {
+    var ctx = canvas.getContext('2d');
+    var width  = 375; var height = 500;
+    canvas.height = height; canvas.width = width;
+    var offset = $(canvas).offset();
     offset.top = offset.top - height;
-});
+    $(window).resize( function() {
+        offset = $(canvas).offset();
+        offset.top = offset.top - height;
+    });
 
-with (ctx) {
-    lineCap = "round";
-    drawImage(image, 0, 0);
-    console.log(image);
-    globalCompositeOperation = "destination-out";
-    lineWidth = 15;
-    strokeStyle = "green";
-}
+    with (ctx) {
+        lineCap = "round";
+        drawImage(image, 0, 0);
+        console.log(image);
+        globalCompositeOperation = "destination-out";
+        lineWidth = 15;
+        strokeStyle = "green";
+    }
 
-image.remove();
+    image.remove();
 
-canvas.addEventListener('mousedown', function(e) {
-    this.down = true;
-    this.X = e.pageX - offset.left;
-    this.Y = e.pageY - offset.top;
-}, 0);
-canvas.addEventListener('mouseup', function() {
-    this.down = false;          
-}, 0);
-canvas.addEventListener('mousemove', function(e) {
-    if (this.down) {
-        ctx.beginPath();
-        ctx.moveTo(this.X, this.Y);
-        ctx.lineTo(e.pageX - offset.left, e.pageY - offset.top);
-        ctx.stroke();
+    canvas.addEventListener('mousedown', function(e) {
+        this.down = true;
         this.X = e.pageX - offset.left;
         this.Y = e.pageY - offset.top;
-    }
-}, 0);
+    }, 0);
+    canvas.addEventListener('mouseup', function() {
+        this.down = false;          
+    }, 0);
+    canvas.addEventListener('mousemove', function(e) {
+        if (this.down) {
+            ctx.beginPath();
+            ctx.moveTo(this.X, this.Y);
+            ctx.lineTo(e.pageX - offset.left, e.pageY - offset.top);
+            ctx.stroke();
+            this.X = e.pageX - offset.left;
+            this.Y = e.pageY - offset.top;
+        }
+    }, 0);
+});
